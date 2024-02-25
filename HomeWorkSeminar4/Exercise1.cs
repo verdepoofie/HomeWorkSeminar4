91a1f37b64ebb90bb14d54a921718622ef0cdce5
daria <dzaytseva@mail.ru>
2024-02-25 16:48:32 +0300

    - usings

--- HomeWorkSeminar4/Exercise1.cs
+++ HomeWorkSeminar4/Exercise1.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HomeWorkSeminar4
+﻿namespace HomeWorkSeminar4
 {
     internal class Exercise1
     {
